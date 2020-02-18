181afa239bf9f7bc8b52ad4338926769162449a4
pdeng <635639654@qq.com>
2020-02-18 16:16:13 +0800

    amd cmd

--- JS/amd-cmd.js
+++ JS/amd-cmd.js
@@ -2,17 +2,26 @@
 !function(){}()
 
 
+// 三大模块化标准
 // AMD->define  CMD->define commonjs->module.exports
 (function(global, factory){
-  if (typeof exports === "object" && typeof module !== "undefined") {
-    module.exports = factory
-  }else {
-    if(typeof define === "function" && define.amd){
-      define(factory)
-    }else{
-      global.biz = factory()
-    }
-  }
+  // if (typeof exports === "object" && typeof module !== "undefined") {
+  //   module.exports = factory()
+  // }else {
+  //   if(typeof define === "function" && define.amd){
+  //     define(factory)
+  //   }else{
+  //     global.biz = factory()
+  //   }
+  // }
+
+  typeof exports === "object" && typeof module !== "undefined" 
+  ? module.exports = factory()
+  : typeof define === "function" && define.amd
+    ? define(factory)
+    : (global.biz = factory());
+
+
 })(this, function() {
   return function() {
     console.log('---走你.')
