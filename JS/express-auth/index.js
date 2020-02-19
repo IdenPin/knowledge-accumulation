30dfcfb779b4d6872111a7aa8469b027454995ed
pdeng <635639654@qq.com>
2020-02-20 00:58:37 +0800

    集成 swagger ui

--- JS/express-auth/index.js
+++ JS/express-auth/index.js
@@ -1,78 +1,44 @@
-const { User } = require('./db')
 const express = require('express')
-const JWT = require("jsonwebtoken");
+
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 // 实例化express创建服务
 const app = new express();
-const PRIVATE_KEY = 'justdoit'
+app.use(express.json());
+require('./routes')(app)
 
-app.use(express.json())
 
-app.get('/', function(req, res) {
-  res.send("<h3>This is a express auth project demo .</h3>");
-})
 
-app.get('/api/users', async function(req, res){
-  const data = await User.find()
+
+const swaggerDefinition = {
+  info: {
+    title: "express auth demo",
+    version: 0.1,
+    description: "express auth user 接口文档"
+  },
+  host: "http://localhost:3000",
+  basePath: '/'
+};
+
+
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
   res.send({
-    code: 0,
-    data: data,
-    msg: "用户列表获取成功"
+    swaggerDefinition,
+    apis: ["./routes.js"]
   });
 })
 
-app.post("/api/register", async function(req, res) {
-  await User.create({
-    username: req.body.username,
-    password: req.body.password
-  });
-  res.send({
-    code: 0,
-    data: null,
-    msg: '注册成功'
-  })
-});
-
-app.post("/api/login", async function(req, res) {
-  const data = await User.findOne({
-    username: req.body.username
-  })
-  if(data === null) {
-    res.status(422).send({
-      code: 1,
-      data: data,
-      msg: "用户名错误"
-    });
-  }else{
-    const isValid = require('bcryptjs').compareSync(req.body.password, data.password)
-    // 生成 token
-    const token = JWT.sign({
-      id: data._id
-    }, PRIVATE_KEY) 
-    res.send({
-      code: isValid ? 0 : 1,
-      data: isValid ? { username: data.username, id: data._id } : null,
-      token,
-      msg: isValid ? "登录成功" : "密码错误"
-    });
-  }
-});
-
-const authMiddleWare = function(req, res) {
-  const token = req.headers.authorization.split("Bearer ").pop();
-  req.token = token
-}
-
-app.get('/api/profile',authMiddleWare, async function(req, res){
-  const {id} = JWT.verify(req.token, PRIVATE_KEY)
-  const data = await User.findById(id)
-  res.send({
-    code: 0,
-    data: data,
-    msg: "用户个人信息返回成功"
-  });
-})
-
-
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJSDoc({
+      swaggerDefinition,
+      apis: ["./routes.js"]
+    })
+  )
+);
 
 app.listen(3000, function() {
   console.log('服务启动了, http://localhost:3000')
---
+++ JS/express-auth/routes.js
@@ -0,0 +1,81 @@
+
+module.exports = (app) => {
+
+const { User } = require("./db");
+const JWT = require("jsonwebtoken");
+const PRIVATE_KEY = "justdoit";
+
+
+  
+  app.get("/", function(req, res) {
+    res.send("<h3>This is a express auth project demo .</h3>");
+  });
+
+  app.get("/api/users", async function(req, res) {
+    const data = await User.find();
+    res.send({
+      code: 0,
+      data: data,
+      msg: "用户列表获取成功"
+    });
+  });
+
+  app.post("/api/register", async function(req, res) {
+    await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
+    res.send({
+      code: 0,
+      data: null,
+      msg: "注册成功"
+    });
+  });
+
+  
+  app.post("/api/login", async function(req, res) {
+    const data = await User.findOne({
+      username: req.body.username
+    });
+    if (data === null) {
+      res.status(422).send({
+        code: 1,
+        data: data,
+        msg: "用户名错误"
+      });
+    } else {
+      const isValid = require("bcryptjs").compareSync(
+        req.body.password,
+        data.password
+      );
+      // 生成 token
+      const token = JWT.sign(
+        {
+          id: data._id
+        },
+        PRIVATE_KEY
+      );
+      res.send({
+        code: isValid ? 0 : 1,
+        data: isValid ? { username: data.username, id: data._id } : null,
+        token,
+        msg: isValid ? "登录成功" : "密码错误"
+      });
+    }
+  });
+
+  const authMiddleWare = function(req, res, next) {
+    req.token = req.headers.authorization.split("Bearer ").pop();
+    next()
+  };
+
+  app.get("/api/profile", authMiddleWare, async function(req, res) {
+    const { id } = JWT.verify(req.token, PRIVATE_KEY);
+    const data = await User.findById(id);
+    res.send({
+      code: 0,
+      data: data,
+      msg: "用户个人信息返回成功"
+    });
+  });
+}
