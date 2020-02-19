9d2db3e55be045466dc6c5da7b3567d39e81f735
pdeng <635639654@qq.com>
2020-02-20 00:14:31 +0800

    express 结合 bcrypt 和 jsonwebtoken 鉴权过程

--- JS/express-auth/index.js
+++ JS/express-auth/index.js
@@ -1,5 +1,6 @@
 const { User } = require('./db')
 const express = require('express')
+const JWT = require("jsonwebtoken");
 // 实例化express创建服务
 const app = new express();
 const PRIVATE_KEY = 'justdoit'
@@ -36,23 +37,37 @@ app.post("/api/login", async function(req, res) {
     username: req.body.username
   })
   if(data === null) {
-    res.send({
+    res.status(422).send({
       code: 1,
       data: data,
       msg: "用户名错误"
     });
   }else{
-    const isSuccess = require('bcryptjs').compareSync(req.body.password, data.password)
-    const token = require('jsonwebtoken').sign({
+    const isValid = require('bcryptjs').compareSync(req.body.password, data.password)
+    // 生成 token
+    const token = JWT.sign({
       id: data._id
     }, PRIVATE_KEY) 
     res.send({
-      code: isSuccess ? 0 : 1,
-      data: isSuccess ? { username: data.username, token: token } : null,
-      msg: isSuccess ? "登录成功" : "密码错误"
+      code: isValid ? 0 : 1,
+      data: isValid ? { username: data.username, id: data._id } : null,
+      token,
+      msg: isValid ? "登录成功" : "密码错误"
     });
   }
 });
+
+
+app.get('/api/profile', async function(req, res){
+  const token = req.headers.authorization.split("Bearer ").pop()
+  const {id} = JWT.verify(token, PRIVATE_KEY)
+  const data = await User.findById(id)
+  res.send({
+    code: 0,
+    data: data,
+    msg: "用户个人信息返回成功"
+  });
+})
 
 
 
