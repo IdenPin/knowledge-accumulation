d9253673a3d49c9ae591a56da24fb8c71b8377fe
pdeng <635639654@qq.com>
2020-04-26 22:37:09 +0800

    发布订阅 Dep/Watcher

--- JS/vue/MVVM.js
+++ JS/vue/MVVM.js
@@ -1,3 +1,48 @@
+// 观察者（发布订阅） 
+class Dep {
+  constructor() {
+    // 存放所有的 watcher
+    this.subs = []
+  }
+  // 订阅（添加 watcher）
+  addSub(watcher) {
+    this.subs.push(watcher)
+  }
+  // 发布
+  notify() {
+    this.subs.forEach(watcher => watcher.update())
+  }
+}
+
+
+
+
+class Watcher {
+  constructor(vm, expr, cb) {
+    this.vm = vm
+    this.expr = expr
+    this.cb = cb
+    // 默认先存放一个老值
+    this.oldVal = this.get()
+  }
+  get() {
+    Dep.target = this
+    let value = CompileUtil.getVal(this.vm, this.expr)
+    Dep.target = null 
+    return value
+  }
+  update() {
+    // 更新操作 数据变化后 会调用观察者的 update 方法
+    let newVal = CompileUtil.getVal(this.vm, this.expr)
+    if(newVal !== this.oldVal) {
+      this.cb(newVal)
+    }
+  }
+}
+
+
+
+
 class Observer {
   constructor(data) {
     this.observer(data)
@@ -12,15 +57,19 @@ class Observer {
   }
   defineReactive(data, key, value) {
     this.observer(value)
+    // 给每个属性都加上一个具有发布订阅的功能
+    let dep = new Dep()
     Object.defineProperty(data, key, {
       get(){
+        // 创建 watcher 时，会去到对应的内容， 并且把 watcher 放到全局
+        Dep.target && dep.addSub(Dep.target)
         return value
       },
       set: (newVal) =>{
-        console.log(newVal, value)
         if(newVal !== value){
           this.observer(newVal)
           value = newVal
+          dep.notify()
         }
       }
     })
@@ -104,20 +153,53 @@ CompileUtil = {
       return acc[v]
     }, vm.$data)
   },
+  // vm.$data 'item.msg' = 'pdeng'
+  setVal(vm, expr, value) {
+    return expr.split('.').reduce((acc, v, index, arr) => {
+      if(arr.length === index + 1) {
+        //如果是最后一项
+        return acc[v] = value 
+      }
+      return acc[v]
+    }, vm.$data)
+  },
   // 节点、表达式、当前实例
   model(node, expr, vm) {
-    // expr -> tmie.msg
+    // expr -> time.msg
+    let fn = this.updater['modelUpdater']
+    // 给输入框添加一个观察者，如果稍后数据更新了会触发此方法
+    // 会拿新值给输入框赋值
+    new Watcher(vm, expr, (newVal) => {
+      fn(node, newVal)
+    })
+    node.addEventListener('input', (e) => {
+      let value = e.target.value
+      this.setVal(vm, expr, value)
+    })
     let value = this.getVal(vm, expr)
-    this.updater['modelUpdater'](node, value)
+    fn(node, value)
   },
   html() {
 
   },
-  text(node, expr, vm){ // {{a}} {{b}}
-    let value = expr.replace(/\{\{(.+?)\}\}/g, (...arg) => {
+  getContentVal(vm, expr) {
+    // 遍历表达式 将内容重新替换成一个完整的内容返回回去
+    return expr.replace(/\{\{(.+?)\}\}/g, (...arg) => {
       return this.getVal(vm, arg[1])
     })
-    this.updater['textUpdater'](node, value)
+  },
+  text(node, expr, vm){ // {{a}} {{b}}
+    let fn = this.updater['textUpdater']
+    // expr -> {{age}} {{time.name}}
+    let content = expr.replace(/\{\{(.+?)\}\}/g, (...arg) => {
+      // 给表达式中每个{{}}都添加观察者
+      new Watcher(vm, arg[1], () => {
+        // 返回一个全的字符串
+        fn(node, this.getContentVal(vm, expr))
+      })
+      return this.getVal(vm, arg[1])
+    })
+    fn(node, content)
   },
   updater: {
     // 把数据插入到节点中
@@ -137,8 +219,25 @@ class Vue {
     if(this.$el) {
       // 使用 Object.defineProperty 重新定义数据
       new Observer(this.$data)
+      // 把 vm 上的取值操作都代理到 vm.$data 上
+      this.proxyVm(this.$data)
+
+
       // 编译模板
       new Compiler(this.$el, this)
+    }
+  }
+  proxyVm(data) {
+    // 38.17
+    for(let key in data) {
+      Object.defineProperty(this, key, {
+        get() {
+          return data[key]
+        },
+        set(newVal){
+          data[key] = newVal
+        }
+      })
     }
   }
 }
